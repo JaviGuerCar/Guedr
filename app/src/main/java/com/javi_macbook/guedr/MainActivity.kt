f26b020d794f38ca1eaefeac285e28c241161a67
Fco. Javier Guerrero Carmona <fjguerreroc@icloud.com>
2017-11-03 00:28:46 +0100

    Tags, SavedInstanceState, FrameLayout

--- app/src/main/java/com/javi_macbook/guedr/MainActivity.kt
+++ app/src/main/java/com/javi_macbook/guedr/MainActivity.kt
@@ -2,6 +2,7 @@ package com.javi_macbook.guedr
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +14,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         Log.v(TAG, "He pasado por onCreate")
 
+        if (savedInstanceState != null) {
+            Log.v(TAG, "SavedInstanceState no es null y su clave vale: ${savedInstanceState.getString("clave")}")
+        }
+        else{
+            Log.v(TAG, "SavedInstanceState es null")
+        }
     }
 
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        Log.v(TAG, "He pasado por onSaveInstanceState")
+
+  /*      if (outState != null) {
+            // Aqui estamos seguros de poder llamar a métodos sin NPE
+            outState.putString("clave", "valor")
+        }*/
+
+        outState?.putString("clave", "valor")
+    }
 }
