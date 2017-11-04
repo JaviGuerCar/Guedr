b9e859017966e3188f483de33733d8df6b80b220
Fco. Javier Guerrero Carmona <fjguerreroc@icloud.com>
2017-11-04 12:11:38 +0100

    Example of ConstraintLayout

--- app/src/main/java/com/javi_macbook/guedr/ForecastActivity.kt
+++ app/src/main/java/com/javi_macbook/guedr/ForecastActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.ImageView
 import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_main.*
 
 class ForecastActivity : AppCompatActivity() {
 
@@ -31,7 +30,7 @@ class ForecastActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_forecast)
 
         forecast = Forecast(25f, 10f, 35f, "Soleado con alguna nube", R.drawable.ico_01)
 
---
+++ app/src/main/java/com/javi_macbook/guedr/SettingsActivity.kt
@@ -0,0 +1,15 @@
+package com.javi_macbook.guedr
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by Javi-MacBook on 3/11/17.
+ */
+class SettingsActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
